7b0529a4be3327df5794a5fc63dd2dbb0d4ffcf3
zweistein22 <30336416+zweistein22@users.noreply.github.com>
2022-10-16 22:09:27 +0200

    Update install-pikrellcam.sh

--- install-pikrellcam.sh
+++ install-pikrellcam.sh
@@ -124,7 +124,7 @@ PACKAGE_LIST=""
 if ((DEB_VERSION >= BUSTER))
 then
 	AV_PACKAGES="ffmpeg"
-	PHP_PACKAGES="php7.3 php7.3-common php7.3-fpm"
+	PHP_PACKAGES="php7.4 php7.4-common php7.4-fpm"
 elif ((DEB_VERSION >= STRETCH))
 then
 	AV_PACKAGES="libav-tools"
@@ -315,7 +315,7 @@ sudo sed -i "s|PIKRELLCAM_WWW|$PWD/www|; \
 
 if ((DEB_VERSION >= BUSTER))
 then
-	sudo sed -i "s/php5/php\/php7.3/" /etc/nginx/sites-available/pikrellcam
+	sudo sed -i "s/php5/php\/php7.4/" /etc/nginx/sites-available/pikrellcam
 elif ((DEB_VERSION >= STRETCH))
 then
 	sudo sed -i "s/php5/php\/php7.0/" /etc/nginx/sites-available/pikrellcam
