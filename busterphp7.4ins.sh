7dacf3fd0b67a7a50298a68b3272f59d0ec1343e
Andreas Langhoff <andreas.langhoff@gmail.com>
2024-05-26 16:22:59 +0200

    .

--- busterphp7.4ins.sh
+++
@@ -1,8 +0,0 @@
-wget https://www.php.net/distributions/php-7.4.33.tar.gz
-tar -xf php-7.4.33.tar.gz
-cd php-7.4.33
-sudo apt-get install -y libxml2-dev libsqlite3-dev
-./configure
-./make -j4
-sudo make install
-
